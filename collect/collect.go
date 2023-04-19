53e214a5d1961dc4c2f0c261f45c78cb7e48bedf
beiyougufen <2270423705@qq.com>
2023-04-19 15:53:05 +0800

    feat: context

---
+++ collect/collect.go
@@ -0,0 +1,93 @@
+package collect
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+)
+
+type Fetcher interface {
+	Get(url string) ([]byte, error)
+}
+
+type BaseFetch struct {
+}
+
+func (BaseFetch) Get(url string) ([]byte, error) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("url: %v, status code: %v", url, resp.StatusCode)
+		return nil, err
+	}
+
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+
+	return ioutil.ReadAll(utf8Reader)
+}
+
+type BrowserFetch struct {
+	Timeout time.Duration
+}
+
+// 模拟浏览器访问
+func (b BrowserFetch) Get(url string) ([]byte, error) {
+
+	client := &http.Client{
+		Timeout: b.Timeout,
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("new req failed: %w", err)
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("url: %v, status code: %v", url, resp.StatusCode)
+		return nil, err
+	}
+
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+
+	bytes, err := r.Peek(1024)
+
+	if err != nil {
+		fmt.Printf("fetch error: %v", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+
+	return e
+}
--- main.go
+++ main.go
@@ -1,69 +1,35 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
+	"github.com/beiyougufen/crawler/collect"
 )
 
 func main() {
-	url := "https://www.thepaper.cn"
-	body, err := Fetch(url)
-
+	url := "https://book.douban.com/subject/1007305/"
+	var f collect.Fetcher = collect.BrowserFetch{
+		Timeout: 10 * time.Second,
+	}
+	body, err := f.Get(url)
 	if err != nil {
-		fmt.Printf("url: %v, read body failed: %v", url, err)
+		fmt.Printf("read content failed: %v", err)
 		return
 	}
+	fmt.Println(string(body))
 
+	// 加载HTML文档
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
 	if err != nil {
-		fmt.Printf("url: %v, goquery.NewDocumentFromReader failed: %v", url, err)
-		return
+		fmt.Printf("read content failed: %v", err)
 	}
 
-	doc.Find("div.new_li h2 a[target=_blank]").Each(func(i int, s *goquery.Selection) {
+	doc.Find("div.news_li h2 a[target=_blank]").Each(func(i int, s *goquery.Selection) {
+		// 获取匹配元素的文本
 		title := s.Text()
 		fmt.Printf("Review %d: %s\n", i, title)
 	})
-}
-
-func Fetch(url string) ([]byte, error) {
-
-	resp, err := http.Get(url)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("url: %v, status code: %v", url, resp.StatusCode)
-	}
-
-	bodyReader := bufio.NewReader(resp.Body)
-	e := DeterminEncoding(bodyReader)
-	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
-	return ioutil.ReadAll(utf8Reader)
-}
-
-func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
-
-	bytes, err := r.Peek(1024)
-
-	if err != nil {
-		fmt.Printf("fetch error: %v", err)
-		return unicode.UTF8
-	}
-
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
 }
